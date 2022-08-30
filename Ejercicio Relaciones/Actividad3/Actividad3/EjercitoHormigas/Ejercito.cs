cb273dfcb124fdbdd697af45220450945ca29218
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-08-30 01:00:39 -0500

    Chore: Actividad 3 Relaciones y interface

---
+++ Ejercicio Relaciones/Actividad3/Actividad3/EjercitoHormigas/Ejercito.cs
@@ -0,0 +1,33 @@
+﻿
+using Actividad3.Hormigas;
+using Actividad3.Tipos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad3.EjercitoHormigas
+{
+    internal class Ejercito
+    {
+        private IHormiguero Hormiguero;
+
+        private Hormiga HormigaExplorador;
+
+        private Hormiga HormigaSoldado;
+
+        private IHormiguero HormigaObrera;
+
+        public Ejercito(IHormiguero hormiguero, IHormiguero hormigaObrera)
+        {
+            Hormiguero = hormiguero;
+
+            this.HormigaExplorador = new HormigaExplorador();
+
+            this.HormigaSoldado = new HormigaSoldado();
+
+            HormigaObrera = hormigaObrera;
+        }
+    }
+}
---
+++ Ejercicio Relaciones/Actividad3/Actividad3/HormigaObrera.cs
@@ -0,0 +1,23 @@
+﻿using Actividad3.Hormigas;
+using Actividad3.Tipos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad3
+{
+    internal class HormigaObrera : Hormiga, IHormiguero
+    {
+        public void Construir()
+        {
+            Console.WriteLine("contruye hormiguero en cualquien lugar");
+        }
+
+        public override void Vigilar()
+        {
+            Console.WriteLine("Vigila para recolectar objetos para el hormiguero");
+        }
+    }
+}
