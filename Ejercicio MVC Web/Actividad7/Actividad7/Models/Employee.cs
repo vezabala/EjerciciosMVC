195997842c9abedeeaa6cf9ba554f89d907dc1b2
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-09-05 15:06:09 -0500

    chore: Ejercicio mvc

---
+++ Ejercicio MVC Web/Actividad7/Actividad7/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿namespace Actividad7.Models
+{
+    public class Employee :Entity
+    {
+        public string Identification { get; private set; }
+
+        public string Name { get; private set; }
+
+        public DateTime FechaDeIngreso { get; private set; }
+
+        public Guid EnterpriseId { get; private set; }
+
+        public Enterprise Enterprise { get; private set; }
+
+        /// <summary>
+        /// for
+        /// </summary>
+        public Employee() :base()
+        {
+
+        }
+
+        public Employee(Guid id, string identification, string name, DateTime fechaDeIngreso,
+            Guid enterpriseId) : base(id)
+        {
+            Identification = identification;
+            Name = name;
+            FechaDeIngreso = fechaDeIngreso;
+            EnterpriseId = enterpriseId;
+        }
+
+        public Employee Build(Guid id, string identification, string name, 
+            DateTime fechaDeIngreso, Guid enterpriseId)
+        {
+            return new Employee(id, identification, name, fechaDeIngreso, enterpriseId);
+        }
+    }
+}
---
+++ Ejercicio MVC Web/Actividad7/Actividad7/Models/Entity.cs
@@ -0,0 +1,21 @@
+﻿namespace Actividad7.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        /// <summary>
+        /// for
+        /// </summary>
+        protected Entity()
+        {
+
+        }
+
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+
+    }
+}
