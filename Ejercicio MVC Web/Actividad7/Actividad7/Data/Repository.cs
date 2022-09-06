d2c7345b020a6962fa699ca564de6bf6ff6f5201
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-09-06 12:08:12 -0500

    chore: Data Actividad 7 Proyecto final

---
+++ Ejercicio MVC Web/Actividad7/Actividad7/Data/Repository.cs
@@ -0,0 +1,40 @@
+﻿using Actividad7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actividad7.Data
+{
+    public class Repository : IRepository
+    {
+        private readonly Actividad7Context context;
+
+        public Repository(Actividad7Context context)
+        {
+            this.context = context;
+        }
+
+        public async Task Commit()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public void Delete<T>(T obj) where T : Entity
+        {
+            context.Set<T>().Remove(obj);
+        }
+
+        public async Task<List<T>> GetAll<T>() where T : Entity
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task Save<T>(T obj) where T : Entity
+        {
+            await context.Set<T>().AddAsync(obj);
+        }
+
+        public void Update<T>(T obj) where T : Entity
+        {
+            context.Set<T>().Update(obj);
+        }
+    }
+}
