2fd4888df6e2a91523a5668ec639021080844455
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-09-06 10:55:54 -0500

    chore:  Explicacion MVC Web Data Repository

--- explicaciones/mvc web/MVCWEB/MVCWEB/Data/IRepositoy.cs
+++ explicaciones/mvc web/MVCWEB/MVCWEB/Data/IRepositoy.cs
@@ -4,12 +4,14 @@ namespace MVCWEB.Data
 {
     public interface IRepositoy
     {
-        public void Save<T>(T obj) where T : Entity;
+        public Task Save<T>(T obj) where T : Entity;
 
         public void Delete<T>(T obj) where T : Entity;
 
         public void Update<T>(T obj) where T : Entity;
 
-        public List<T> GetAll<T>() where T : Entity;
+        public Task<List<T>> GetAll<T>() where T : Entity;
+
+        public Task Commit();
     }
 }
--- explicaciones/mvc web/MVCWEB/MVCWEB/Data/Repository.cs
+++ explicaciones/mvc web/MVCWEB/MVCWEB/Data/Repository.cs
@@ -1,6 +1,40 @@
-﻿namespace MVCWEB.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MVCWEB.Models;
+
+namespace MVCWEB.Data
 {
-    public class Repository
+    public class Repository : IRepositoy
     {
+        private readonly MVCWEBContext context;
+
+        public Repository(MVCWEBContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task Commit()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public void Delete<T>(T obj) where T : Entity
+        {
+            context.Set<T>().Remove(obj);
+        }
+
+        public async Task<List<T>> GetAll<T>() where T : Entity
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task Save<T>(T obj) where T : Entity
+        {
+            await context.Set<T>().AddAsync(obj);
+        }
+
+        public void Update<T>(T obj) where T : Entity
+        {
+            context.Set<T>().Update(obj);
+        }
     }
 }
